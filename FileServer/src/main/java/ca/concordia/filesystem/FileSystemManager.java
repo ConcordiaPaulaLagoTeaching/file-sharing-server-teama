2d0b516d444aa66e3e1833056839e8a97b3f62ca
dylanp3802 <dylanptl2002@gmail.com>
2025-11-13 20:50:10 -0500

    Version 1.4 (finished createfile)

--- FileServer/src/main/java/ca/concordia/filesystem/FileSystemManager.java
+++ FileServer/src/main/java/ca/concordia/filesystem/FileSystemManager.java
@@ -1,17 +1,16 @@
-//Version 1.2
+//Version 1.4
 
 package ca.concordia.filesystem;
 
 import ca.concordia.filesystem.datastructures.FEntry;
-import ca.concordia.filesystem.datastructures.FNode;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class FileSystemManager {
 
+    private final int FILEENTRYSIZE = 15; //string (11 bytes) + 2xshort (4bytes)
     private final int MAXFILES = 5;
     private final int MAXBLOCKS = 10;
     private final static FileSystemManager instance = null;
@@ -20,7 +19,7 @@ public class FileSystemManager {
 
     private static final int BLOCK_SIZE = 128; // Example block size
 
-    private FEntry[] inodeTable; // Array of inodes
+    private FEntry[] fileEntryDescriptors; // Array of inodes
     private boolean[] freeBlockList; // Bitmap for free blocks (0 empty, 1 taken)
 
     public FileSystemManager(String filename, int totalSize) {
@@ -29,7 +28,12 @@ public class FileSystemManager {
         if(instance == null) {
 
             try {
-                //TODO Initialize the file system
+                        //initialize the array containing all the File entries
+                        fileEntryDescriptors = new FEntry[MAXFILES];
+
+                        //initialize the free block list array
+                        freeBlockList = new boolean[MAXBLOCKS];
+
 
                         //virtual memory mode and length
                         this.disk = new RandomAccessFile(filename, "rw");
@@ -55,46 +59,67 @@ public class FileSystemManager {
 
     public void createFile(String fileName) throws Exception {
 
+        int saveIndex = -1;
+        int diskOffset = 0;
 
-        freeBlockList = new boolean[MAXBLOCKS]; //initialize size for freeBlocklist
-        int indexfree=0;
+        //1) create a new file entry
+        FEntry newFile = new FEntry(fileName, (short)0, (short)-1);
 
-
-        //1)run algorithm to check next available spot in blocklist using the bitmap
-        for (int i=0; i<MAXBLOCKS; i++) {
-            if(!freeBlockList[i]){ //if block is free
-                indexfree = i; //save free index
+        try {
+            //2) check the file entry array to see if there's available space
+            for (int i = 0; i < fileEntryDescriptors.length; i++) {
+                if (fileEntryDescriptors[i] == null) {
+                    saveIndex = i;
+                    break;
+                }
             }
+            if (saveIndex == -1) {
+                throw new Exception("Error: No more space for files");
+            }
+            else {
+                fileEntryDescriptors[saveIndex] = newFile;
+
+            }
+
+            //3) calculate offset to write the next file entry
+            diskOffset = saveIndex * FILEENTRYSIZE;
+
+
+            //3) write metadata starting from offset in disk
+            disk.seek(diskOffset);
+
+            //add padding to name of file to keep file entry structure in order
+            String filenamePadded;
+            int padding = 0;
+            filenamePadded = newFile.getFilename();
+
+            if (filenamePadded.length() < 11) {
+
+                padding = 11 - filenamePadded.length();
+                for (int i = 0; i < padding; i++) {
+                    filenamePadded = filenamePadded + " ";
+                }
+            }
+
+            disk.writeBytes(filenamePadded);
+            disk.writeShort(newFile.getFilesize());
+            disk.writeShort(newFile.getFirstBlock());
+
         }
 
-
-
-
-
-            //2)create file entry
-        FEntry newfile = new FEntry(fileName, BLOCK_SIZE, );
-
-
-        //place metadata in array
-        for (int i=0; i<inodeTable.length; i++) {
-
-            if(inodeTable[i] == null) {
-                inodeTable[i] = newfile;
-
+        catch (IOException e){
+            if (saveIndex != -1) {
+                fileEntryDescriptors[saveIndex] = null;
             }
-            else{
-                System.out.println("no empty space");
-            }
+
+            throw new Exception("Disk write failed");
         }
-
-        //set location in freeblocklist to 1 (spot now taken)
-
-        //allocate space on virtual disk
-        disk.write();
-
-
-        throw new UnsupportedOperationException("Method not implemented yet.");
     }
+
+
+
+
+
 
 
     // TODO: Add readFile, writeFile and other required methods,
